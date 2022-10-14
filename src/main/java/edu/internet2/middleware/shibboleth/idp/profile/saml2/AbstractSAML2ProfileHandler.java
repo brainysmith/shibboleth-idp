8eaa798c4cc8233ca7f2d879bf8b840753dfb9ea
gam <agumerov@identityblitz.com>
2022-10-14 16:18:11 +0300

    IB-2478: changing IdP Signature Method Algorithm

--- src/main/java/edu/internet2/middleware/shibboleth/idp/profile/saml2/AbstractSAML2ProfileHandler.java
+++ src/main/java/edu/internet2/middleware/shibboleth/idp/profile/saml2/AbstractSAML2ProfileHandler.java
@@ -581,9 +581,6 @@ public abstract class AbstractSAML2ProfileHandler extends AbstractSAMLProfileHan
         Credential signatureCredential = profileConfig.getSigningCredential();
         if (signatureCredential == null) {
             signatureCredential = requestContext.getRelyingPartyConfiguration().getDefaultSigningCredential();
-            log.info("####requestContext.getRelyingPartyConfiguration signatureCredential '{}'",signatureCredential.getCredentialType().getCanonicalName());
-        } else {
-            log.info("####AbstractSAML2ProfileConfiguration signatureCredential '{}'", signatureCredential.getCredentialType().getCanonicalName());
         }
 
         if (signatureCredential == null) {
@@ -597,16 +594,11 @@ public abstract class AbstractSAML2ProfileHandler extends AbstractSAMLProfileHan
 
         log.debug("Signing assertion to relying party {}", requestContext.getInboundMessageIssuer());
         Signature signature = signatureBuilder.buildObject(Signature.DEFAULT_ELEMENT_NAME);
-        log.info("####Signature.getSignatureAlgorithm '{}'", signature.getSignatureAlgorithm());
 
         signature.setSigningCredential(signatureCredential);
         try {
             // TODO pull SecurityConfiguration from SAMLMessageContext? needs to be added
             // TODO how to pull what keyInfoGenName to use?
-            org.opensaml.xml.security.BasicSecurityConfiguration conf = (org.opensaml.xml.security.BasicSecurityConfiguration) org.opensaml.xml.Configuration.getGlobalSecurityConfiguration();
-            log.info("####GlobalSecurityConfiguration '{}'", conf);
-            log.info("####GlobalSecurityConfiguration.getSignatureAlgorithmURI for RSA'{}'", conf.getSignatureAlgorithmURI("RSA"));
-            log.info("####GlobalSecurityConfiguration.getSignatureAlgorithmURI '{}'", conf.getSignatureAlgorithmURI(signatureCredential));
             SecurityHelper.prepareSignatureParams(signature, signatureCredential, null, null);
         } catch (SecurityException e) {
             requestContext.setFailureStatus(buildStatus(StatusCode.RESPONDER_URI, null,
